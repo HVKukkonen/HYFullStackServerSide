f861c85c4b0678f385e70593a160c9bf65764520
Kukkonen <valtteri.kukkonen@aalto.fi>
2020-10-24 12:38:22 +0300

    uibuild

--- index.js
+++ index.js
@@ -28,6 +28,8 @@ notes = [
 const cors = require('cors')
 app.use(cors())
 
+app.use(express.static('build'))
+
 const morgan = require('morgan')
 morgan.token('reqbody', function(req, res) {return JSON.stringify(req.body)})
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :reqbody"))
