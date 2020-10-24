446d96f467bc940dffeb9981dea4f94e91c26ea1
Kukkonen <valtteri.kukkonen@aalto.fi>
2020-10-24 12:30:22 +0300

    uibuild

---
+++ build/precache-manifest.cf1015a7df3a5b474c3186927c6ba07a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5da6465e505dd626dd47445c202d8a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d47da239019ef2478bb6",
+    "url": "/static/css/main.87de7a9e.chunk.css"
+  },
+  {
+    "revision": "18d829bd9815d71cccb6",
+    "url": "/static/js/2.b6c08380.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/static/js/2.b6c08380.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d47da239019ef2478bb6",
+    "url": "/static/js/main.565c7e57.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
