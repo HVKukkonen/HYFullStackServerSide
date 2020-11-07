06a2ff37b270d55c8ff72ca98fc025868462e8ac
Kukkonen <valtteri.kukkonen@aalto.fi>
2020-11-07 10:51:51 +0200

    uibuild

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.get('/persons/:id', (request, response) => {
 app.delete('/persons/:id', (request, response) => {
     //const id = Number(request.params.id)  // request id from server
     // notes = notes.filter(note => note.id !== id)
-    console.log('Note: ', Note)
+    //console.log('Note: ', Note)
     notes.findByIdAndRemove(request.params.id)
         .then(result => {response.status(204).end()})
 })
